8e6b09c2279fcdf5278fcb688ce4c0b09030c21a
Matheus Ferreira <teteu.andrade06@gmail.com>
2022-06-25 19:46:27 -0300

    Setting cors

--- src/http/server.js
+++ src/http/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { routes } = require("./routes");
+const cors = require("cors");
 require("../database");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
 // Import routes
